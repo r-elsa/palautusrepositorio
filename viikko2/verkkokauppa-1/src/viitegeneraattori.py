0e96346908ce57f8a834327742390ec7d3dd1f3e
Elsa <elsaryynanen@outlook.com>
2022-11-14 22:12:28 +0200

    tehtävä 11

--- viikko2/verkkokauppa-1/src/viitegeneraattori.py
+++ viikko2/verkkokauppa-1/src/viitegeneraattori.py
@@ -1,12 +1,5 @@
 class Viitegeneraattori:
-    __instanssi = None
 
-    @staticmethod
-    def get_instance():
-        if not Viitegeneraattori.__instanssi:
-            Viitegeneraattori.__instanssi = Viitegeneraattori()
-
-        return Viitegeneraattori.__instanssi
 
     def __init__(self):
         self._seuraava = 1
